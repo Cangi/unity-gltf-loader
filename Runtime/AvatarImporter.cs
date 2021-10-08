8846295ec582e74e012b1092b2881c4d5e6da926
Sercan Altundas <sercan.altundas@gmail.com>
2021-10-08 13:37:54 +0300

    init

---
+++ Runtime/AvatarImporter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using Siccity.GLTFUtility;
+
+namespace ReadyPlayerMe
+{
+	public class AvatarImporter
+	{
+		private static readonly ImportSettings Settings = new ImportSettings() { useLegacyClips = false };
+
+		public static void ImportAsync(byte[] bytes, Action<GameObject> onImportFinished)
+		{
+			Importer.ImportGLBAsync(bytes, Settings, onImportFinished);
+		}
+
+		public static GameObject Import(byte[] bytes)
+		{
+			return Importer.LoadFromBytes(bytes, Settings);
+		}
+	}
+}
---
+++ Samples/GLTFLoaderSample.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using ReadyPlayerMe;
+
+public class GLTFLoaderSample : MonoBehaviour
+{
+	// Start is called before the first frame update
+	private void Start()
+	{
+		// Using glb file with .bytes extension in Resources folder here
+		// You can replace the importer parameter with result of web request
+		TextAsset asset = Resources.Load<TextAsset>("avatar");
+
+		// Import asynchronously
+		AvatarImporter.ImportAsync(asset.bytes, OnImportFinished);
+
+		// Import directly, locks main thread
+		GameObject avatar = AvatarImporter.Import(asset.bytes);
+		avatar.name = "Loaded Direct";
+		avatar.transform.position = new Vector3(0.3f, 0, 0);
+	}
+	
+	private void OnImportFinished(GameObject avatar)
+	{
+		avatar.name = "Loaded Async";
+		avatar.transform.position = new Vector3(-0.3f, 0, 0);
+	}
+}
